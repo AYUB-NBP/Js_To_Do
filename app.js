aeb946a28d529e1c4dfb1e6d9260a60db776213f
Ayoub <supaboy96@gmail.com>
2022-05-16 22:21:22 +0100

    So far so Good, added basic Functionality with one debug.

---
+++ app.js
@@ -0,0 +1,10 @@
+document.querySelector('#form').onsubmit = () => {
+    let newItem = document.createElement('li'); //NewLi
+    if (document.querySelector('#input').value != '') {
+        newItem.innerHTML = document.querySelector('#input').value;//Filled NewLi with input value
+        document.getElementById('List').appendChild(newItem);/*Inserted newItem to list*/
+    }
+    else { document.querySelector('#input').placeholder = 'Type something.' }
+    document.querySelector('#input').value = ''; //Cleared InputField
+    return false;
+}
