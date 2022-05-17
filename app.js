d27dc7c04b3b2f428e1f56e7a7e29778fbc60604
Ayoub <supaboy96@gmail.com>
2022-05-17 04:06:37 +0100

    i can clear correctly, but still I can't remove single elements

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-document.querySelector('#form').onsubmit = () => {
+//Clear Local Storage
+
+
+document.querySelector('#form').onsubmit = () => { /*Add new Item */
     let newItem = document.createElement('li'); //NewLi
     if (document.querySelector('#input').value != '') {
         newItem.innerHTML = document.querySelector('#input').value;//Filled NewLi with input value
         document.getElementById('List').appendChild(newItem);/*Inserted newItem to list*/
+        history.go(0);
     }
     else { document.querySelector('#input').placeholder = 'Type something.' }
+
     document.querySelector('#input').value = ''; //Cleared InputField
+
+
 
     //Local Storage Save
     localStorage.setItem('listItems', document.getElementById('List').innerHTML)
@@ -13,3 +20,5 @@ document.querySelector('#form').onsubmit = () => {
 }
 
 var listArr = document.getElementById('List').childNodes;
+
+document.getElementById('clear').onclick = () => { window.localStorage.clear(); history.go(0); return false; }
