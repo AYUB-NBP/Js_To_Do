9470d83b024e9b5f36efcd399dd9054fee125891
Ayoub <supaboy96@gmail.com>
2022-05-21 08:44:48 +0100

    Stuck in the remove Buttons ugh

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 
 // Form Submission
+
 document.querySelector('#form').onsubmit = () => {
-    let newItem = document.createElement('li'); //NewLi
-    let newBtn = document.createElement('button'); //NewBtn
+    //New list item
+    let newItem = document.createElement('li');
+    //NewBtn (removing button for each list item)
+    var newBtn = document.createElement('button');
+    //Adding class to newBtn
+    newBtn.classList.add('rmvBtn');
+    //Remove Buttons Array
+    rmvBtns = document.querySelectorAll('.rmvBtn')
+    //Item Removal Function
+    function removeListItem() { console.log('Event working'); }
+    //Button Filler
     newBtn.innerHTML = 'X';
 
-    
+
+
     if (document.querySelector('#input').value != '') {
-        newItem.innerHTML = document.querySelector('#input').value;//Filled NewLi with input value
-        document.getElementById('List').appendChild(newItem);/*Inserted newItem to list*/
+        //Fill NewLi with input value
+        newItem.innerHTML = document.querySelector('#input').value;
+        //Inserted newItem to list
+        document.getElementById('List').appendChild(newItem);
         newItem.appendChild(newBtn);
         history.go(0);
     }
     else { document.querySelector('#input').placeholder = 'Type something.' }
 
-    document.querySelector('#input').value = ''; //Cleared InputField
-
+    //Cleared InputField
+    document.querySelector('#input').value = '';
 
 
     //Local Storage Save
     localStorage.setItem('listItems', document.getElementById('List').innerHTML)
-    return false; // Prevent refresh after submission
+
+    // Prevent refresh after submission
+    return false;
 }
 
-var listArr = document.getElementById('List').childNodes;
-
+// Clear All Button function
 document.getElementById('clear').onclick = () => { window.localStorage.clear(); history.go(0); return false; }//Clear Local Storage
 
