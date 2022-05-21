724cce424ef916c1ae2735832e8a2a51ad586eb0
Ayoub <supaboy96@gmail.com>
2022-05-21 07:00:57 +0100

    Added remove buttons & some minimal styling

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-//Clear Local Storage
 
-
-document.querySelector('#form').onsubmit = () => { /*Add new Item */
+// Form Submission
+document.querySelector('#form').onsubmit = () => {
     let newItem = document.createElement('li'); //NewLi
+    let newBtn = document.createElement('button'); //NewBtn
+    
+    
     if (document.querySelector('#input').value != '') {
         newItem.innerHTML = document.querySelector('#input').value;//Filled NewLi with input value
         document.getElementById('List').appendChild(newItem);/*Inserted newItem to list*/
+        newItem.appendChild(newBtn);
         history.go(0);
     }
     else { document.querySelector('#input').placeholder = 'Type something.' }
@@ -21,4 +24,5 @@ document.querySelector('#form').onsubmit = () => { /*Add new Item */
 
 var listArr = document.getElementById('List').childNodes;
 
-document.getElementById('clear').onclick = () => { window.localStorage.clear(); history.go(0); return false;}
+document.getElementById('clear').onclick = () => { window.localStorage.clear(); history.go(0); return false; }//Clear Local Storage
+
